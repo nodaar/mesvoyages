585df2c74ffbf19ee4124455cc44304d96d86c9c
Alexandre <alexandre.darnault@gmail.com>
2021-09-09 15:48:15 +0200

    Phase 8:  ORM (affichage en colonne des informations provenant de la BDD)

--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -85,6 +85,11 @@ class Visite
     {
         return $this->datecreation;
     }
+    
+    public function getDatecreationString(): string
+    {
+        return $this->datecreation->format('d/m/Y');
+    }
 
     public function setDatecreation(?\DateTimeInterface $datecreation): self
     {
