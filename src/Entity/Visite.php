9ded4ab0743296fd72f43c67ac58aae5816b173b
Alexandre <alexandre.darnault@gmail.com>
2021-09-29 20:37:36 +0200

    Phase 23: Tests

--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -37,11 +37,13 @@ class Visite
 
     /**
      * @ORM\Column(type="date", nullable=true)
+     * @Assert\LessThanOrEqual("now")
      */
     private $datecreation;
 
     /**
      * @ORM\Column(type="integer", nullable=true)
+     * @Assert\Range(min = 0, max = 20)
      */
     private $note;
 
@@ -57,6 +59,7 @@ class Visite
 
     /**
      * @ORM\Column(type="integer", nullable=true)
+     * @Assert\GreaterThan(propertyPath="tempmin")
      */
     private $tempmax;
     
